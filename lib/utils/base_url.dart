17e169cd6acc148602db999e4a4733367cf2ecc9
candra <candraherdiansyah14@gmail.com>
2023-01-13 20:12:12 +0700

    detail article 25%

--- lib/utils/base_url.dart
+++ lib/utils/base_url.dart
@@ -1,4 +1,4 @@
 class ApiUrl {
-  static String root = 'https://81cf-180-244-128-248.ngrok.io'; //dynamic link
+  static String root = 'http://f725-180-244-128-248.ngrok.io'; //dynamic link
   static String baseApi = '${root}/api/v1';
 }
